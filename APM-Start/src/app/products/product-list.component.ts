d1551e5e9f210ee40924600c73d691f0e70d4949
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2020-04-07 18:11:49 -0300

    Added Product Interface/editorconfig single quote

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,35 +1,36 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
-  selector: "pm-products",
-  templateUrl: "./product-list.component.html",
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
-  pageTitle: string = "Product List!";
+  pageTitle: string = 'Product List!';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = "cart";
-  products: any[] = [
+  listFilter: string = 'cart';
+  products: IProduct[] = [
     {
       productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
       price: 19.95,
       starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png",
+      imageUrl: 'assets/images/leaf_rake.png',
     },
     {
       productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
+      productName: 'Garden Cart',
+      productCode: 'GDN-0023',
+      releaseDate: 'March 18, 2019',
+      description: '15 gallon capacity rolling garden cart',
       price: 32.99,
       starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png",
+      imageUrl: 'assets/images/garden_cart.png',
     },
   ];
 
